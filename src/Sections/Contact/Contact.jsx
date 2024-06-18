43e3eea4c1ff16a5f37713d0235fb7dc12345676
kiranjith Hardeep <160715253+kiranjithhardeepp@users.noreply.github.com>
2024-06-18 12:13:17 +0530

    final
    added emailjs

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -1,10 +1,45 @@
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
 import styles from './ContactStyles.module.css';
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    emailjs.send(
+      'service_b7cmh27',
+      'template_bm0igng',
+      formData,
+      'hqCquPmTif8SDh5KK'
+    )
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+      alert('Message sent successfully!');
+      setFormData({ name: '', email: '', message: '' });  // Reset the form data
+    })
+    .catch((err) => {
+      console.error('FAILED...', err);
+      alert('Failed to send message. Please try again later.');
+    });
+  };
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="formGroup">
           <label htmlFor="name" hidden>
             Name
@@ -15,6 +50,8 @@ function Contact() {
             id="name"
             placeholder="Name"
             required
+            value={formData.name}
+            onChange={handleChange}
           />
         </div>
         <div className="formGroup">
@@ -27,6 +64,8 @@ function Contact() {
             id="email"
             placeholder="Email"
             required
+            value={formData.email}
+            onChange={handleChange}
           />
         </div>
         <div className="formGroup">
@@ -37,7 +76,10 @@ function Contact() {
             name="message"
             id="message"
             placeholder="Message"
-            required></textarea>
+            required
+            value={formData.message}
+            onChange={handleChange}
+          ></textarea>
         </div>
         <input className="hover btn" type="submit" value="Submit" />
       </form>
