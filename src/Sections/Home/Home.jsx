20150379f8db01fe15b4e35d6858eb91a445c9ce
kiranjithhardeepp <Kiranjithardeep@gmail.com>
2024-08-10 12:06:49 +0530

    inital commit

--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -9,7 +9,7 @@ import githubLight from "../../assets/github-light.svg";
 import githubDark from "../../assets/github-dark.svg";
 import linkedinLight from "../../assets/linkedin-light.svg";
 import linkedinDark from "../../assets/linkedin-dark.svg";
-import CV from "../../assets/KiranjithHardeep.pdf";
+import CV from "../../assets/Kiranjithhardeep.pdf";
 import { useTheme } from "../../Common/ThemeContext";
 
 function Hero() {
